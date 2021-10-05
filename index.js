e49bbe17e374c79a82840d967b5934c4ec0ada9b
NMunro <neilmunro@gmail.com>
2021-10-05 16:29:15 +0100

    Updated to include envvars

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
+
 const argv = yargs(hideBin(process.argv)).argv;
 const {add, list, update, remove} = require("./utils/");
 
--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
+const path = require("path");
 const { nanoid, customAlphabet } = require("nanoid");
+const dataPath = path.join(process.env.DATA_LOCATION, "data.json");
 
 const saveData = (data) => {
     try {
-        fs.writeFileSync("data.json", JSON.stringify(data));
+        if (!fs.existsSync(process.env.DATA_LOCATION)) {
+            fs.mkdirSync(process.env.DATA_LOCATION);
+        }
+
+        fs.writeFileSync(dataPath, JSON.stringify(data));
     } catch(error) {
         console.log(error);
     }
@@ -11,27 +17,29 @@ const saveData = (data) => {
 
 const loadData = () => {
     try {
-        return JSON.parse(fs.readFileSync("data.json").toString());
+        return JSON.parse(fs.readFileSync(dataPath).toString());
     } catch(error) {
         return [];
     }
 };
 
-const makeID = () => customAlphabet("0123456789abcdef", 10)();
+const makeID = () => customAlphabet(process.env.CHARACTERS, parseInt(process.env.LENGTH))();
 
 const add = (title, artist, album, id = false) => saveData([...loadData(), {id: id || makeID(), title, artist, album}]);
 
-const list = (title, artist, album) => console.log(loadData());
+const list = () => console.log(loadData());
 
 const update = (id, title, artist, album) => {
-    remove(id);
-    add(title, artist, album, id);
+    const song = remove(id);
+    add(title || song.title, artist || song.artist, album || song.album, id);
 };
 
 const remove = (id) => {
-    const matchSong = (song) => song.id !== id;
     const songs = loadData();
+    const song = songs.find((song) => song.id === id);
+    const matchSong = (song) => song.id !== id;
     saveData(songs.filter(matchSong));
+    return song;
 };
 
 module.exports = {add, list, update, remove};
